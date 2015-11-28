9ccccdb804dd8af3e13c88cde9d8b58c24d3b60f
Ankush Aggarwal <ankushagg93@gmail.com>
2015-11-28 01:56:57 -0500

    modularize onPerformSync

--- app/src/main/java/com/aggarwalankush/interview2go/sync/InterviewSyncAdapter.java
+++ app/src/main/java/com/aggarwalankush/interview2go/sync/InterviewSyncAdapter.java
@@ -21,7 +21,6 @@ import com.aggarwalankush.interview2go.TopicFragment;
 import com.aggarwalankush.interview2go.data.InterviewContract.InterviewEntry;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 
 import java.io.BufferedReader;
@@ -43,8 +42,8 @@ public class InterviewSyncAdapter extends AbstractThreadedSyncAdapter {
     public static final int SYNC_INTERVAL = 5 * 24 * 60 * 60;
     public static final int SYNC_FLEXTIME = SYNC_INTERVAL / 3;
 
-    boolean flag = true;
-    boolean saveBookMarks = false;
+    boolean updateProgressTitle = true;
+    boolean emptyDB = false;
 
     public InterviewSyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
@@ -53,25 +52,158 @@ public class InterviewSyncAdapter extends AbstractThreadedSyncAdapter {
 
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
-//        Log.d(LOG_TAG, "onPerformSync Called.");
 
-        Cursor checkEmptyDB = getContext().getContentResolver().query(InterviewEntry.CONTENT_URI, new String[]{InterviewEntry.COLUMN_BOOKMARK}, null, null, null);
+        TreeMap<String, TreeMap<String, String>> topicToQueToOutput;
+        Vector<ContentValues> cVVector = new Vector<>();
 
-        if ((checkEmptyDB != null) && (checkEmptyDB.getCount() > 0)) {
-            saveBookMarks = true;
-            checkEmptyDB.close();
+        final String SERVER_BASE_URL = getContext().getString(R.string.server_base_url);
+        final String SERVER_OUTPUT_FILE = getContext().getString(R.string.server_output_file);
+        final String SERVER_LIGHT_SOLUTION = getContext().getString(R.string.server_light_solution);
+        final String SERVER_DARK_SOLUTION = getContext().getString(R.string.server_dark_solution);
+
+        // Output.json file handling
+        Uri outputJsonUri = Uri.parse(SERVER_BASE_URL).buildUpon()
+                .appendPath(SERVER_OUTPUT_FILE)
+                .build();
+        String outputJsonStr = getDataFromServer(outputJsonUri);
+        topicToQueToOutput = jsonToMap(outputJsonStr);
+
+
+        for (Entry<String, TreeMap<String, String>> stringTreeMapEntry : topicToQueToOutput.entrySet()) {
+            String topic = stringTreeMapEntry.getKey();
+            for (Entry<String, String> quesToOutputEntry : stringTreeMapEntry.getValue().entrySet()) {
+                String question = quesToOutputEntry.getKey();
+                String output = quesToOutputEntry.getValue();
+                String solution = "Invalid Solution";
+                String darkSolution = "Invalid Solution";
+                int bookmark = getBookmark(topic, question);
+
+                // light solution handling
+                Uri lightSolutionUri = Uri.parse(SERVER_BASE_URL).buildUpon()
+                        .appendPath(SERVER_LIGHT_SOLUTION)
+                        .appendPath(topic)
+                        .appendPath(question)
+                        .build();
+                String light_solution = getDataFromServer(lightSolutionUri);
+                if (null != light_solution) {
+                    solution = light_solution;
+                }
+
+                // dark solution handling
+                Uri darkSolutionUri = Uri.parse(SERVER_BASE_URL).buildUpon()
+                        .appendPath(SERVER_DARK_SOLUTION)
+                        .appendPath(topic)
+                        .appendPath(question)
+                        .build();
+                String dark_solution = getDataFromServer(darkSolutionUri);
+                if (null != dark_solution) {
+                    darkSolution = dark_solution;
+                }
+
+                // adding values to content values
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(InterviewEntry.COLUMN_TOPIC, topic);
+                contentValues.put(InterviewEntry.COLUMN_QUESTION, question);
+                contentValues.put(InterviewEntry.COLUMN_SOLUTION, solution);
+                contentValues.put(InterviewEntry.COLUMN_DARK_SOLUTION, darkSolution);
+                contentValues.put(InterviewEntry.COLUMN_OUTPUT, output);
+                contentValues.put(InterviewEntry.COLUMN_BOOKMARK, bookmark);
+                cVVector.add(contentValues);
+
+                // update progress bar
+                updateProgressBar();
+            }
         }
+
+        // insert into database
+        if (cVVector.size() > 0) {
+            ContentValues[] cvArray = new ContentValues[cVVector.size()];
+            cVVector.toArray(cvArray);
+            getContext().getContentResolver().bulkInsert(InterviewEntry.CONTENT_URI, cvArray);
+        }
+
+    }
+
+    public TreeMap<String, TreeMap<String, String>> jsonToMap(String outputJsonStr) {
+        TreeMap<String, TreeMap<String, String>> topicToQueToOutput = new TreeMap<>();
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = jsonParser.parse(outputJsonStr).getAsJsonObject();
+
+        for (Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+            String topic = entry.getKey();
+            JsonObject quesSolObj = jsonParser.parse(entry.getValue().getAsString()).getAsJsonObject();
+            TreeMap<String, String> quesToOutput = new TreeMap<>();
+            for (Entry<String, JsonElement> quesOutputEntry : quesSolObj.entrySet()) {
+                String question = quesOutputEntry.getKey();
+                String solution = quesOutputEntry.getValue().getAsString();
+                quesToOutput.put(question, solution);
+            }
+            topicToQueToOutput.put(topic, quesToOutput);
+        }
+        return topicToQueToOutput;
+    }
+
+
+    public void updateProgressBar() {
+        if (TopicFragment.handler != null && TopicFragment.progressDialog != null) {
+            TopicFragment.handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    TopicFragment.progressDialog.incrementProgressBy(1);
+                    if (updateProgressTitle) {
+                        TopicFragment.progressDialog.setTitle(getContext().getString(R.string.progress_title));
+                        TopicFragment.progressDialog.setMessage(getContext().getString(R.string.progress_message));
+                        updateProgressTitle = false;
+                    }
+                }
+            });
+        }
+    }
+
+    public int getBookmark(String topic, String question) {
+        int bookmark = 0;
+        if (!isEmptyDB()) {
+            Cursor cursor = getContext().getContentResolver().query(
+                    InterviewEntry.buildTopicWithQuestion(topic, question),
+                    new String[]{InterviewEntry.COLUMN_BOOKMARK},
+                    null,
+                    null,
+                    null
+            );
+            if ((cursor != null) && (cursor.getCount() > 0)) {
+                cursor.moveToFirst();
+                bookmark = cursor.getInt(0);
+                cursor.close();
+            }
+        }
+        return bookmark;
+    }
+
+    public boolean isEmptyDB() {
+        if (emptyDB) {
+            return true;
+        }
+        Cursor cursor = getContext().getContentResolver().query(
+                InterviewEntry.CONTENT_URI,
+                new String[]{InterviewEntry.COLUMN_BOOKMARK},
+                null,
+                null,
+                null);
+
+        if (null == cursor || cursor.getCount() == 0) {
+            emptyDB = true;
+        } else {
+            cursor.close();
+        }
+        return emptyDB;
+    }
+
+    public String getDataFromServer(Uri builtUri) {
 
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
 
-        TreeMap<String, TreeMap<String, String>> topicToQueToOutput = new TreeMap<>();
-
         try {
-            final String SERVER_BASE_URL = getContext().getString(R.string.server_base_url);
-            String server_output_file = getContext().getString(R.string.server_output_file);
-
-            Uri builtUri = Uri.parse(SERVER_BASE_URL).buildUpon().appendPath(server_output_file).build();
             URL url = new URL(builtUri.toString());
 
             urlConnection = (HttpURLConnection) url.openConnection();
@@ -83,7 +215,7 @@ public class InterviewSyncAdapter extends AbstractThreadedSyncAdapter {
             StringBuilder builder = new StringBuilder();
             if (inputStream == null) {
                 // Nothing to do.
-                return;
+                return null;
             }
             reader = new BufferedReader(new InputStreamReader(inputStream));
 
@@ -93,27 +225,12 @@ public class InterviewSyncAdapter extends AbstractThreadedSyncAdapter {
             }
 
             if (builder.length() == 0) {
-                return;
+                return null;
             }
-            String outputJsonStr = builder.toString();
-            JsonParser jsonParser = new JsonParser();
-            JsonObject jsonObject = jsonParser.parse(outputJsonStr).getAsJsonObject();
 
-            for (Entry<String, JsonElement> entry : jsonObject.entrySet()) {
-                String topic = entry.getKey();
-                JsonObject quesSolObj = jsonParser.parse(entry.getValue().getAsString()).getAsJsonObject();
-                TreeMap<String, String> quesToOutput = new TreeMap<>();
-                for (Entry<String, JsonElement> quesOutputEntry : quesSolObj.entrySet()) {
-                    String question = quesOutputEntry.getKey();
-                    String solution = quesOutputEntry.getValue().getAsString();
-                    quesToOutput.put(question, solution);
-                }
-                topicToQueToOutput.put(topic, quesToOutput);
-            }
+            return builder.toString();
         } catch (IOException e) {
-            Log.e(LOG_TAG, "IO Error ", e);
-        } catch (JsonParseException e) {
-            Log.e(LOG_TAG, "Json Parse error ", e);
+            Log.e(LOG_TAG, "IO Exception", e);
         } finally {
             if (urlConnection != null) {
                 urlConnection.disconnect();
@@ -126,154 +243,9 @@ public class InterviewSyncAdapter extends AbstractThreadedSyncAdapter {
                 }
             }
         }
-
-        Vector<ContentValues> cVVector = new Vector<>();
-
-        for (Entry<String, TreeMap<String, String>> stringTreeMapEntry : topicToQueToOutput.entrySet()) {
-            String topic = stringTreeMapEntry.getKey();
-            TreeMap<String, String> quesToOutput = stringTreeMapEntry.getValue();
-            for (Entry<String, String> quesToOutputEntry : quesToOutput.entrySet()) {
-                String question = quesToOutputEntry.getKey();
-                String output = quesToOutputEntry.getValue();
-                String solution = "Invalid Solution";
-                String darkSolution = "Invalid Solution";
-                int bookmark = 0;
-                try {
-                    final String SERVER_BASE_URL = getContext().getString(R.string.server_base_url);
-                    String server_light_solution = getContext().getString(R.string.server_light_solution);
-                    Uri builtUri = Uri.parse(SERVER_BASE_URL).buildUpon().appendPath(server_light_solution).appendPath(topic).appendPath(question).build();
-                    URL url = new URL(builtUri.toString());
-
-                    urlConnection = (HttpURLConnection) url.openConnection();
-                    urlConnection.setRequestMethod("GET");
-                    urlConnection.connect();
-
-                    InputStream inputStream = urlConnection.getInputStream();
-                    StringBuilder builder = new StringBuilder();
-                    if (inputStream == null) {
-                        return;
-                    }
-                    reader = new BufferedReader(new InputStreamReader(inputStream));
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        builder.append(line);
-                    }
-
-                    if (builder.length() == 0) {
-                        return;
-                    }
-                    solution = builder.toString();
-
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "IO Error ", e);
-                } finally {
-                    if (urlConnection != null) {
-                        urlConnection.disconnect();
-                    }
-                    if (reader != null) {
-                        try {
-                            reader.close();
-                        } catch (final IOException e) {
-                            Log.e(LOG_TAG, "Error closing stream", e);
-                        }
-                    }
-                }
-
-                try {
-                    final String SERVER_BASE_URL = getContext().getString(R.string.server_base_url);
-                    String server_dark_solution = getContext().getString(R.string.server_dark_solution);
-                    Uri builtUri = Uri.parse(SERVER_BASE_URL).buildUpon().appendPath(server_dark_solution).appendPath(topic).appendPath(question).build();
-                    URL url = new URL(builtUri.toString());
-
-                    urlConnection = (HttpURLConnection) url.openConnection();
-                    urlConnection.setRequestMethod("GET");
-                    urlConnection.connect();
-
-                    InputStream inputStream = urlConnection.getInputStream();
-                    StringBuilder builder = new StringBuilder();
-                    if (inputStream == null) {
-                        return;
-                    }
-                    reader = new BufferedReader(new InputStreamReader(inputStream));
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        builder.append(line);
-                    }
-
-                    if (builder.length() == 0) {
-                        return;
-                    }
-                    darkSolution = builder.toString();
-
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "IO Error ", e);
-                } finally {
-                    if (urlConnection != null) {
-                        urlConnection.disconnect();
-                    }
-                    if (reader != null) {
-                        try {
-                            reader.close();
-                        } catch (final IOException e) {
-                            Log.e(LOG_TAG, "Error closing stream", e);
-                        }
-                    }
-                }
-
-                if (saveBookMarks) {
-                    Cursor cursor = getContext().getContentResolver().query(
-                            InterviewEntry.buildTopicWithQuestion(topic, question),
-                            new String[]{InterviewEntry.COLUMN_BOOKMARK},
-                            null,
-                            null,
-                            null
-                    );
-                    if ((cursor != null) && (cursor.getCount() > 0)) {
-                        cursor.moveToFirst();
-                        bookmark = cursor.getInt(0);
-//                        Log.d(LOG_TAG, "bookmark is " + bookmark);
-                        cursor.close();
-                    }
-
-                } else {
-                    if (TopicFragment.handler != null && TopicFragment.progressDialog != null) {
-                        TopicFragment.handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                TopicFragment.progressDialog.incrementProgressBy(1);
-                                if (flag) {
-                                    TopicFragment.progressDialog.setTitle(getContext().getString(R.string.progress_title));
-                                    TopicFragment.progressDialog.setMessage(getContext().getString(R.string.progress_message));
-                                    flag = false;
-                                }
-                            }
-                        });
-                    }
-                }
-
-
-                ContentValues contentValues = new ContentValues();
-                contentValues.put(InterviewEntry.COLUMN_TOPIC, topic);
-                contentValues.put(InterviewEntry.COLUMN_QUESTION, question);
-                contentValues.put(InterviewEntry.COLUMN_SOLUTION, solution);
-                contentValues.put(InterviewEntry.COLUMN_DARK_SOLUTION, darkSolution);
-                contentValues.put(InterviewEntry.COLUMN_OUTPUT, output);
-                contentValues.put(InterviewEntry.COLUMN_BOOKMARK, bookmark);
-
-                cVVector.add(contentValues);
-
-
-            }
-        }
-
-        if (cVVector.size() > 0) {
-            ContentValues[] cvArray = new ContentValues[cVVector.size()];
-            cVVector.toArray(cvArray);
-            getContext().getContentResolver().bulkInsert(InterviewEntry.CONTENT_URI, cvArray);
-        }
-
-//        Log.d(LOG_TAG, "Interview Sync Complete. " + cVVector.size() + " Inserted");
+        return null;
     }
+
 
 //    public static void syncImmediately(Context context) {
 //        Bundle bundle = new Bundle();
