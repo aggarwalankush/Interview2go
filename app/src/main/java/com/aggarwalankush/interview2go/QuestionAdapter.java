1fa0232aaf3dabf1177b3e20f4dea782fc2190f3
Ankush Aggarwal <ankushagg93@gmail.com>
2016-03-31 01:32:43 -0700

    added question detail template - just have to get question detail from database

--- app/src/main/java/com/aggarwalankush/interview2go/QuestionAdapter.java
+++ app/src/main/java/com/aggarwalankush/interview2go/QuestionAdapter.java
@@ -1,12 +1,19 @@
 package com.aggarwalankush.interview2go;
 
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.MeasureSpec;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.aggarwalankush.interview2go.QuestionAdapter.QuestionAdapterViewHolder;
@@ -67,12 +74,91 @@ public class QuestionAdapter extends RecyclerView.Adapter<QuestionAdapterViewHol
     public class QuestionAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public final TextView mQuestionView;
         public final ImageView mIconView;
+        public final TextView mQuestionDetailView;
+        public final LinearLayout mQuestionDetailLinearLayout;
+        public final FrameLayout mQuestionIconLayout;
 
         public QuestionAdapterViewHolder(View view) {
             super(view);
             mIconView = (ImageView) view.findViewById(R.id.list_item_icon);
             mQuestionView = (TextView) view.findViewById(R.id.tv_question);
+            mQuestionDetailView = (TextView) view.findViewById(R.id.tv_question_detail);
+            mQuestionDetailLinearLayout = (LinearLayout) view.findViewById(R.id.ll_question_detail);
+            mQuestionIconLayout = (FrameLayout) view.findViewById(R.id.question_detail_icon);
+            mQuestionDetailLinearLayout.setVisibility(View.GONE);
+
+            mQuestionIconLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mQuestionDetailLinearLayout.getVisibility()==View.GONE){
+                        expand();
+                    }else{
+                        collapse();
+                    }
+                }
+            });
+
             view.setOnClickListener(this);
+        }
+
+
+        private void expand() {
+            //set Visible
+            mQuestionDetailLinearLayout.setVisibility(View.VISIBLE);
+
+            final int widthSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+            final int heightSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+            mQuestionDetailLinearLayout.measure(widthSpec, heightSpec);
+
+            ValueAnimator mAnimator = slideAnimator(0, mQuestionDetailLinearLayout.getMeasuredHeight());
+            mAnimator.start();
+        }
+
+        private void collapse() {
+            int finalHeight = mQuestionDetailLinearLayout.getHeight();
+
+            ValueAnimator mAnimator = slideAnimator(finalHeight, 0);
+
+            mAnimator.addListener(new AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    //Height=0, but it set visibility to GONE
+                    mQuestionDetailLinearLayout.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+            mAnimator.start();
+        }
+
+        private ValueAnimator slideAnimator(int start, int end) {
+
+            ValueAnimator animator = ValueAnimator.ofInt(start, end);
+
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                    //Update Height
+                    int value = (Integer) valueAnimator.getAnimatedValue();
+                    LayoutParams layoutParams = mQuestionDetailLinearLayout.getLayoutParams();
+                    layoutParams.height = value;
+                    mQuestionDetailLinearLayout.setLayoutParams(layoutParams);
+                }
+            });
+            return animator;
         }
 
         @Override
